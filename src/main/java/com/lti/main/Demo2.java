351d99ceb875ab3ddc7c962a048ff316dc9d1915
Mangesh <mangesh.dhotre.computer.2018@vpkbiet.org>
2022-09-12 22:07:30 +0530

    First Commit

---
+++ src/main/java/com/lti/main/Demo2.java
@@ -0,0 +1,13 @@
+package com.lti.main;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+	  
+		System.out.println("Hello Jenkins How are you?");
+		System.out.println("- | -");
+		System.out.println(" ___ ");
+
+	}
+
+}
